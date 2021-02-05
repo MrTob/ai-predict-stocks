4223c75bc1e4f63dc92e1a86d5dcdb56b2fb260d
MrTob <mr.tobtv@gmail.com>
2021-02-05 17:22:10 +0100

    added libs & collect data

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import pandas_datareader as pdr
+import datetime as dt
+#from sklearn.preprocessing import MinMaxScaler
+#from tensorflow.keras.models import Sequential
+#from tensorflow.keras.layers import Dense, Dropout, LSTM
+
+# load the data
+company = 'FB'
+start = dt.datetime(2010,1,1)
+end = dt.datetime(2020,1,1)
+data = pdr.DataReader(company,'yahoo',start,end)
+
 
